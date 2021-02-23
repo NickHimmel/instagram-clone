793a784095e2dc50e8065d445819af2e5047d5b9
NickHimmel <himmel.nick@gmail.com>
2021-02-23 16:51:46 -0500

    Build login in page and start building sign up page

--- src/App.js
+++ src/App.js
@@ -13,19 +13,17 @@ const NotFound = lazy(() => import ('./pages/not-found'));
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Suspense fallback={<p>Loading... </p>}>
-          <Switch>
-            <Route path={ROUTES.DASHBOARD} component={Dashboard} exact/>
-            <Route path={ROUTES.LOGIN} component={Login}/>
-            <Route path={ROUTES.SIGN_UP} component={SignUp}/>
-            <Route path={ROUTES.PROFILE} component={Profile}/>
-            <Route path={ROUTES.NOT_FOUND} component={NotFound}/>
-          </Switch>
-        </Suspense>
-      </Router>
-    </div>
+    <Router>
+      <Suspense fallback={<p>Loading... </p>}>
+        <Switch>
+          <Route path={ROUTES.DASHBOARD} component={Dashboard} exact/>
+          <Route path={ROUTES.LOGIN} component={Login}/>
+          <Route path={ROUTES.SIGN_UP} component={SignUp}/>
+          <Route path={ROUTES.PROFILE} component={Profile}/>
+          <Route path={ROUTES.NOT_FOUND} component={NotFound}/>
+        </Switch>
+      </Suspense>
+    </Router>
   );
 }
 
